6c8a4cf24dde977977cc2ca2de8e096f9761b7fe
Vishal Sharma <vsjobs72445@gmail.com>
2022-09-15 01:39:39 +0530

    added the port env for heroku

--- problem2.js
+++ problem2.js
@@ -2,8 +2,9 @@ import express from 'express';
 import fs from 'fs';
 
 const app = express();
+const port = process.env.PORT || 4000;
 
-app.listen(4000, () => {
+app.listen(port, () => {
     console.log("Server is running on port 4000.")
 })
 
